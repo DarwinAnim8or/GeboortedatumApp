f990401e602e55241969e68406eb11c08c11a50f
Unknown <gievanom@hotmail.com>
2020-09-28 16:58:29 +0200

    Test code in main

--- GeboortedatumApp/Program.cs
+++ GeboortedatumApp/Program.cs
@@ -3,10 +3,18 @@
 namespace GeboortedatumApp
 {
 	class Program
-	{
+	{ 
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Maak objecten hier:
+			Input input = new Input();
+
+			//Hier komt het welkomst-bericht
+
+			//Tijdelijke test-code:
+			DateTime geboortedatum = input.VraagVoorDatum();
+
+			Console.WriteLine(geboortedatum.ToString());
 		}
 	}
 }
