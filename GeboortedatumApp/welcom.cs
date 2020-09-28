4e3803c136027346ecdb2e8318af9e7dd0e49e1d
vincent-du-jardin <vincent.dujardin@student.ap.be>
2020-09-28 16:58:15 +0200

    welkom aff

--- GeboortedatumApp/welcom.cs
+++ GeboortedatumApp/welcom.cs
@@ -6,5 +6,18 @@ namespace GeboortedatumApp
 {
     class welcom
     {
+        public welcom() 
+        {
+            groet();
+        }
+        public void groet() 
+        {
+            Console.WriteLine("welcome op de leefttijd berekening app  ");
+            Console.WriteLine("u leeftijd word hier na gevraagt het word niet gedeelt ofzo");
+            Console.WriteLine("klinkt enter als je het accepteerd");
+            Console.ReadLine();
+            Console.Clear();
+            
+        }
     }
 }
