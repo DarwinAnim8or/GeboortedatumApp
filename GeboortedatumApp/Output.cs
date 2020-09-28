260d7d2782d2dfedfd75d75f13b16f8e5705706e
vincent-du-jardin <vincent.dujardin@student.ap.be>
2020-09-28 17:04:30 +0200

    begin output

---
+++ GeboortedatumApp/Output.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeboortedatumApp
+{
+    class Output
+    {
+        public Output()
+        {
+            Console.WriteLine();
+        }
+    }
+}
