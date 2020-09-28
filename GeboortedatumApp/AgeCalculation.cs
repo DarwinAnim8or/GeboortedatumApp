992584d993e687ac89d8e556e61f8313a5bb8513
Unknown <gievanom@hotmail.com>
2020-09-28 17:10:52 +0200

    Agecalculation class

---
+++ GeboortedatumApp/AgeCalculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeboortedatumApp
+{
+	class AgeCalculation
+	{
+		private DateTime now;
+
+		public AgeCalculation()
+		{
+			now = DateTime.Today;
+		}
+
+		public int BerekenLeeftijd(DateTime geboorteDatum)
+		{
+			return now.Year - geboorteDatum.Year;
+		}
+	}
+}
